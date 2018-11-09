8b63e65bfc362a2167d3eb53181091a485543aa6
Juan José González Abril <SackCastellon@users.noreply.github.com>
2018-11-09 17:01:30 +0100

    Initial commit

---
+++ src/main/java/es/uji/ei1048/meteorologia/App.java
@@ -0,0 +1,15 @@
+package es.uji.ei1048.meteorologia;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class App extends Application {
+    @Override
+    public void start(final Stage primaryStage) throws Exception {
+        System.out.println("Hello World!");
+    }
+
+    public static void main(final String[] args) {
+        Application.launch(args);
+    }
+}
